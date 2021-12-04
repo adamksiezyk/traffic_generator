3ac9495437f97ccb3e9326001cdd8204f28128d2
Adam Ksiezyk <adam.ksiezyk@gmail.com>
2021-12-04 23:40:05 +0100

    Refactor TaskDTO to deserialize correctly

--- app/src/main/java/com/example/trafficgenerator/dto/TaskDTO.kt
+++ app/src/main/java/com/example/trafficgenerator/dto/TaskDTO.kt
@@ -3,7 +3,7 @@ package com.example.trafficgenerator.dto
 import com.github.kittinunf.fuel.core.ResponseDeserializable
 import com.google.gson.Gson
 
-data class TaskDTO(val id: Long, val type: String) {
+data class TaskDTO(val taskId: Long, val taskType: String) {
     class Deserializer : ResponseDeserializable<TaskDTO> {
         override fun deserialize(content: String): TaskDTO =
             Gson().fromJson(content, TaskDTO::class.java)
